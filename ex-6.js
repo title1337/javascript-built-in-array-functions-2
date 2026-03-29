7bc7f646535ea8bf10b8b822680a2e384ffe9c10
Tle <title1337@gmail.com>
2026-03-29 10:18:56 +0700

    Fix : EX6

--- ex-6.js
+++ ex-6.js
@@ -1,12 +1,19 @@
 const carCollection = ["toyota", "fiat", "honda", "bmw"];
 
 function updateCarCollection(carBrand) {
-  // Start coding here
+  if (carCollection.includes(carBrand)){
+    return `${carBrand} already exists in position ${carCollection.indexOf(carBrand) + 1}
+       of the car collection.`;
+  } else {
+    carCollection.push(carBrand);
+    return `New car collection is ${carCollection}`
+  }
 }
 
 //ผลลัพธ์ที่ควรได้จาก Example case
 //ในกรณีที่ยังไม่มียี่ห้อรถใน carCollection
-console.log(updateCarCollection("audi")); //new car collection is : toyota,fiat,honda,bmw,audi.
-
+console.log(updateCarCollection("gtx")); //new car collection is : toyota,fiat,honda,bmw,audi.
 //ในกรณีที่มียี่ห้อรถใน carCollection
-console.log(updateCarCollection("toyota")); //toyota has already existed in the 1 position of car collection.
+console.log(updateCarCollection("RTX")); //toyota has already existed in the 1 position of car collection.
+
+
