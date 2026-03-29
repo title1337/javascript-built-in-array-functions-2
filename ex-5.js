e0be0efa44c2961568954091ca683972d23e6b2b
Tle <title1337@gmail.com>
2026-03-29 09:52:42 +0700

    Feat : EX5

--- ex-5.js
+++ ex-5.js
@@ -1,7 +1,14 @@
-let crimeRecordNames = ["Theodora Milenko", "Tinashe Benigno", "Mahalia Priyanka", "Maisy Sudhir"];
+let crimeRecordNames = ["Theodora Milenko", 
+  "Tinashe Benigno", 
+  "Mahalia Priyanka", 
+  "Maisy Sudhir"];
 
 function searchCrimeRecord(name) {
-  // Start coding here
+  if (crimeRecordNames.includes){
+    return `${name}  has a crime record!!!`
+  } else {
+    return `${name} has no crime record.`
+  }
 }
 
 //ผลลัพธ์ที่ควรได้จาก Example case
